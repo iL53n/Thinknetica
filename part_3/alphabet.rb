13a5c0cf50076206946dc65e0feae27a224c3e2b
iL53n <ilsegribko@gmail.com>
2019-01-26 15:24:13 +0300

    на проверку

--- part_3/alphabet.rb
+++ part_3/alphabet.rb
@@ -1,9 +1,11 @@
-alphabet = Hash.new
-n = 1
+#4. Заполнить хеш гласными буквами, где значением будет являтся порядковый номер буквы в алфавите (a - 1).
+
+alphabet = Hash.new(0)
+number = 1
 
 ('a'..'z').each do |letter|
-	alphabet[letter] = n
-	n += 1
+	alphabet[letter] = number
+	number += 1
 end
 
 puts alphabet
--- part_3/months.rb
+++ part_3/months.rb
@@ -1,2 +1,21 @@
-months = { January: 31, February: 28, March: 31, April: 30, May: 31, June: 30, July: 31, August: 31, September: 30, October: 31, November: 30, December:31 }
-months.each {|month, days| puts "#{month}" if days == 30 }
+#1. Сделать хеш, содеращий месяцы и количество дней в месяце. 
+#В цикле выводить те месяцы, у которых количество дней ровно 30
+
+#months = { January: 31, February: 28, March: 31, April: 30, May: 31, June: 30, July: 31, August: 31, September: 30, October: 31, November: 30, December:31 }
+
+months = { 
+  January: 31, 
+  February: 28, 
+  March: 31, 
+  April: 30, 
+  May: 31, 
+  June: 30, 
+  July: 31, 
+  August: 31, 
+  September: 30, 
+  October: 31, 
+  November: 30, 
+  December:31 
+}
+
+months.each { |month, days| puts "#{month}" if days == 30 }

be8f0b7a7e3215fb9aca557b1470313cbb5d00a2
iL53n <ilsegribko@gmail.com>
2019-02-21 00:47:55 +0300

    v

--- part_10/accessors.rb
+++ part_10/accessors.rb
@@ -27,7 +27,7 @@ module Accessors
     define_method(name) { instance_variable_get(variable) }
 
     define_method("#{name}=".to_sym) do |value|
-      raise ("Не верный тип для #{name}. Необходим: #{name_class}.") unless value.is_a?(name_class)
+      raise("Не верный тип для #{name}. Необходим: #{name_class}.") unless value.is_a?(name_class)
       instance_variable_set(variable, value)
     end
   end

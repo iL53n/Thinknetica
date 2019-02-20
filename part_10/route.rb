9a9c6c4a983556236e73c816e0cce6923cad39dc
iL53n <ilsegribko@gmail.com>
2019-02-21 00:22:23 +0300

    Update route.rb

--- part_10/route.rb
+++ part_10/route.rb
@@ -1,21 +1,13 @@
 require_relative './instance_counter.rb'
 require_relative './validation.rb'
-<<<<<<< HEAD
-require_relative './accessors'
-=======
->>>>>>> 1e0905092aac27c087aa93f294c9e941b33ce845
+require_relative './accessors.rb'
 
 class Route
   include InstanceCounter
   include Validation
-<<<<<<< HEAD
   extend Accessors
 
   attr_accessors_with_history :stations
-=======
-
-  attr_reader :stations
->>>>>>> 1e0905092aac27c087aa93f294c9e941b33ce845
 
   def initialize(start_station, finish_station)
     @stations = [start_station, finish_station]

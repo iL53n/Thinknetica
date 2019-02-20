be64a3f01d59358478a5f2c4edbf6662e3550c1f
iL53n <ilsegribko@gmail.com>
2019-02-21 00:21:41 +0300

    Update station.rb

--- part_10/station.rb
+++ part_10/station.rb
@@ -1,24 +1,13 @@
 require_relative './instance_counter.rb'
 require_relative './validation.rb'
-<<<<<<< HEAD
 require_relative './accessors'
-=======
->>>>>>> 1e0905092aac27c087aa93f294c9e941b33ce845
 
 class Station
   include InstanceCounter
   include Validation
-<<<<<<< HEAD
   extend Accessors
 
   attr_accessors_with_history :title, :trains
-=======
-
-  EMPTY_TITLE_ERROR = 'Укажите название станции!'.freeze
-  TITLE_FORMAT_ERROR = 'Название станции не может быть меньше 3 символов!'.freeze
-
-  attr_reader :title, :trains
->>>>>>> 1e0905092aac27c087aa93f294c9e941b33ce845
 
   validate :title, :presence
   validate :title, :length
